8878735cca2badfdc713e27fa336e600f74d4e37
Ragnar Thor Valgeirsson <ragnar.valgeirsson@gmail.com>
2015-08-02 15:26:07 +0000

    Drop down manages current blending mode

--- app/components/color-mode.js
+++ app/components/color-mode.js
@@ -3,11 +3,18 @@ export default Ember.Component.extend({
 
   // Probably want to store these differently
   modes: [
-    Ember.Object.create({name: 'Normal', isActive: true}),
-    Ember.Object.create({name: 'Multiply', isActive: false}),
-    Ember.Object.create({name: 'Hard Light', isActive: false}),
-    Ember.Object.create({name: 'Overlay', isActive: false}),
-    Ember.Object.create({name: 'Soft Light', isActive: false})
+    Ember.Object.create({name: 'Normal', func: 'normal', isActive: true}),
+    Ember.Object.create({name: 'Multiply', func: 'multiply', isActive: false}),
+    Ember.Object.create({name: 'Screen', func: 'screen', isActive: false}),
+    Ember.Object.create({name: 'Overlay', func: 'overlay', isActive: false}),
+    Ember.Object.create({name: 'Hard Light', func: 'hardLight', isActive: false}),
+    Ember.Object.create({name: 'Darken', func: 'darken', isActive: false}),
+    Ember.Object.create({name: 'Lighten', func: 'lighten', isActive: false}),
+    Ember.Object.create({name: 'Color Dodge', func: 'colorDodge', isActive: false}),
+    Ember.Object.create({name: 'Color Burn', func: 'colorBurn', isActive: false}),
+    Ember.Object.create({name: 'Soft Light', func: 'softLight', isActive: false}),
+    Ember.Object.create({name: 'Difference', func: 'difference', isActive: false}),
+    Ember.Object.create({name: 'Exclusion', func: 'exclusion', isActive: false})
   ],
 
   currentMode: Ember.computed('modes.@each.isActive', function() {
@@ -39,6 +46,7 @@ export default Ember.Component.extend({
       this.get('currentMode').set('isActive', false);
       mode.set('isActive', true);
       this.set('isOpen', false);
+      this.set('blending', this.get('currentMode').get('func'));
     }
   }
 });
